ef06af8fdce2840433328484ae697b2431cfb038
Lucas Oliveira <lukashamashia@gmail.com>
2023-09-21 20:33:53 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia novo filme do Homem de ferro, confira",
+    "photoCover":"https://es.web.img3.acsta.net/r_654_368/newsv7/20/05/07/12/52/3980724.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série Anunciada no Disney +",
+    "description": "Não  perca mais tempo, confira agora",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+
+]
