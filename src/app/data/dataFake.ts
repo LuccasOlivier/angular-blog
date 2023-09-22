7f04ccf1b86946585ba7ef5c5a63956008602d92
Lucas Oliveira <lukashamashia@gmail.com>
2023-09-21 21:46:55 -0300

    Card Update

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,27 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "Marvel anuncia novo filme do Homem de ferro, confira",
-    "photoCover":"https://es.web.img3.acsta.net/r_654_368/newsv7/20/05/07/12/52/3980724.jpg"
+    "title": "DESCUBRA OS SEGREDOS DA DC COMICS",
+    "description": "Explore o Universo DC através de personagens icônicos como Superman, Batman, Aquaman e Flash, cada um com suas próprias histórias e poderes extraordinários.",
+    "photoCover":"https://cinepop.com.br/wp-content/uploads/2021/09/B4D5531A-9D19-45B4-ACAB-AB93F31D14BC.jpeg"
   },
   {
     "id":"2",
-    "title": "Nova Série Anunciada no Disney +",
-    "description": "Não  perca mais tempo, confira agora",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "EXPLORANDO BATMAN: O CAVALEIRO DAS TREVAS",
+    "description": "Criado por Bob Kane e Bill Finger em 1939, o Batman é um ícone dos quadrinhos e não possui superpoderes. Bruce Wayne, seu alter ego, é um bilionário filantropo que financia suas atividades como vigilante de Gotham City.",
+    "photoCover":"https://images6.alphacoders.com/113/1134871.jpg"
+  },
+   {
+    "id":"3",
+    "title": "O HERÓI KRYPTONIANO: SUPERMAN",
+    "description": "Criado por Jerry Siegel e Joe Shuster em 1938, o Superman é considerado o primeiro super-herói dos quadrinhos. Seu alter ego, Clark Kent, é um repórter no Planeta Diário e usa óculos para ocultar sua identidade secreta.",
+    "photoCover":"https://veja.abril.com.br/wp-content/uploads/2016/06/cinema-homem-de-aco-super-homem-20110823-0002-original1.jpeg?quality=90&strip=info&w=680&h=453&crop=1"
+  },
+  {
+    "id":"4",
+    "title": "REI DOS MARES: AQUAMAN",
+    "description": "Aquaman possui uma origem única, sendo metade humano e metade atlante. Seu poderoso Tridente de Netuno lhe concede o controle sobre os oceanos e é uma das armas mais temidas do Universo DC.",
+    "photoCover":"https://observatoriodocinema.uol.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=1200&height=627&format=webp&quality=91&imagick=/wp-content/uploads/2019/11/1544732958-5c12c17a55928_va_guia_dc_-_aquaman1.jpg"
   }
 
 ]
